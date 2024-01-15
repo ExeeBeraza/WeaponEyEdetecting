dd09f549529ac71c78b90fe45c2d8986a1cb912d
nachoberaza <nachoberaza@gmail.com>
2024-01-15 02:04:13 -0300

    Primera prueba de yolo exitosa.

--- src/yolo_test/detect.py
+++ src/yolo_test/detect.py
@@ -1,16 +1,21 @@
 import torch
 import cv2 as cv
 import numpy as np
+import pathlib
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='Z:/DesarrolloExe/Proyecto_2_IA_2023/models/pistolas.pt')
+# Configuracion requerida para windows
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+model = torch.hub.load('ultralytics/yolov5', 'custom',
+                       path='F:/Dev/AyudaExe/ProyectoFinalIA2023/models/modelo_detector_pistolas.pt')
 
 
-cap = cv.imread('../../resources/pistolas/test.png')
+cap = cv.imread('../../resources/pistolas/glock.jpg')
 
 while True:
-
     # Realizamos deteccion
-    detect = model.detect(cap)
+    detect = model(cap)
 
     # Muestra la imagen en una ventana
     cv.imshow('Detector de pistolas', np.squeeze(detect.render()))
@@ -18,6 +23,5 @@ while True:
     # Espera a que el usuario toque una tecla
     cv.waitKey(0)
     break
-
 
 cv.destroyAllWindows()
